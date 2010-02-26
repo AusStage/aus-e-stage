d675158fb4674e7646618a308b3801384303ae51
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-02-26 04:48:37 +0000

    initial commit of root website

---
+++ trunk/root-web/web/assets/javascript/main.js
@@ -0,0 +1,77 @@
+/*
+ * This file is part of the Aus-e-Stage Beta Root Website
+ *
+ * The Aus-e-Stage Beta Root Website is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The Aus-e-Stage Beta Root Website is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Aus-e-Stage Beta Root Website.  
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// set up the tabbed interface
+$(document).ready(function() {
+	$("#tabs").tabs({ 
+		cookie: { 
+			name: 'aus_e_stage_beta_tab', 
+			expires: 30
+		}
+	}); 
+});
+
+// load the google api
+google.load("feeds", "1");
+
+// load the delicious feed
+google.setOnLoadCallback(load_delicious);
+
+// load the zotero feed
+google.setOnLoadCallback(load_zotero);
+
+// load the delicious rss feed
+function load_delicious() {
+
+	// empty the delicious feed div
+	$("#delicious_feed").empty();
+
+	// Create a feed control
+	var feedControl = new google.feeds.FeedControl();
+	
+	// add the feed url
+	feedControl.addFeed("http://feeds.delicious.com/v2/rss/tag/ausstage?count=15", "");
+	
+	// set the number of items to display
+	feedControl.setNumEntries(10);
+
+	// Draw it.
+	feedControl.draw(document.getElementById("delicious_feed"));
+}
+
+//https://api.zotero.org/groups/12290/items
+
+// load the delicious rss feed
+function load_zotero() {
+
+	// empty the delicious feed div
+	$("#zotero_feed").empty();
+
+	// Create a feed control
+	var feedControl = new google.feeds.FeedControl();
+	
+	// add the feed url
+	feedControl.addFeed("https://api.zotero.org/groups/12290/items/top", "");
+	
+	// set the number of items to display
+	feedControl.setNumEntries(10);
+
+	// Draw it.
+	feedControl.draw(document.getElementById("zotero_feed"));
+}
+
