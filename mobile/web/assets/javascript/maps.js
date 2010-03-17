f3ad654e58f182f7a16522d9735632811e79c3d2
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-03-17 06:28:17 +0000

    incremental improvement of the map interface on the mobile website

--- mobile/web/assets/javascript/maps.js
+++ mobile/web/assets/javascript/maps.js
@@ -16,14 +16,23 @@
  * If not, see <http://www.gnu.org/licenses/>.
 */
 
+// declare global variables
+var map;
+var mapOptions = {
+	zoom: 15,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
 $(document).ready(function() {
 
+	var startLocation;
+	
 	// Try W3C Geolocation (Preferred)
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			// success
-			$('#map_canvas').empty();
-			$('#map_canvas').append('<p>Latitude: ' + position.coords.latitude + '<br/>Longitude: ' + position.coords.longitude + '</p>');
+			startLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);		
+			showMap(startLocation);
 		}, function(position_error) {
 			// failure
 			showError(position_error, 'W3C Geolocation');
@@ -39,8 +48,8 @@ $(document).ready(function() {
 			var geo = google.gears.factory.create('beta.geolocation');
 			geo.getCurrentPosition(function(position) {
 				// success
-				$('#map_canvas').empty();
-				$('#map_canvas').append('<p>Latitude: ' + position.latitude + '<br/>Longitude: ' + position.longitude + '</p>');
+				startLocation = new google.maps.LatLng(position.latitude, position.longitude);
+				showMap(startLocation);
 			}, function(position_error) {
 				//failure
 				// hack to check if we're running in the emulator
@@ -48,14 +57,15 @@ $(document).ready(function() {
 					// running in the emulator so ignore error and fake coordinates
 					//$('#map_canvas').empty();
 					//$('#map_canvas').append('<p>Running in the emulator</p>');
-					showError(position_error, 'Google Gears');
+					// success
+					startLocation = new google.maps.LatLng(138.5727, -35.02597);
+					showMap(startLocation);
 				} else {
 					showError(position_error, 'Google Gears');
 				}
 			}, {
 				// options
-				enableHighAccuracy: true,
-				gearsLocationProviderUrls: null
+				enableHighAccuracy: true
 			});
 		} else {
 			// not available
@@ -76,4 +86,17 @@ function showError(position_error, error_source) {
 	$('#map_canvas').hide();
 	
 	$('#error_form_div').show();
+}
+
+// function to show the map
+function showMap(location) {
+
+	// get a new map object and set to the location we've found
+	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	map.setCenter(location);
+	var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      title:"You Are Here!"
+  });
 }
