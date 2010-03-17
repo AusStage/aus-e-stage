788176996ae8770912f658049e3c908a454265c7
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-03-17 22:51:56 +0000

    minor bugfix to the FileListFilter class of the exchange analytics app

--- trunk/exchange-analytics/run.sh
+++ trunk/exchange-analytics/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to ease compiling by inlcuding class path
 MYPATH=`pwd`
-OUTPUT_PATH=$MYPATH/output/analytics-report.xml
+OUTPUT_PATH=$MYPATH/output/exchange-analytics.xml
 cd $MYPATH/bin
 /bin/rm $OUTPUT_PATH
 /usr/bin/java -cp .:$MYPATH/lib/sqlitejdbc-v056.jar ExchangeAnalytics $MYPATH/log-files $MYPATH/output/analytics.db $OUTPUT_PATH
