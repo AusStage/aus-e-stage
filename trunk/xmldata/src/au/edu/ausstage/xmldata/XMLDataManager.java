877b00ccaf44c4b4783bf6bafc075b0d111255c7
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-05-05 23:41:03 +0000

    added base files and directories for the AusStage XML Data Manager Service

---
+++ trunk/xmldata/src/au/edu/ausstage/xmldata/XMLDataManager.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the AusStage XML Data Manager Service
+ *
+ * The AusStage XML Data Manager Service is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU General 
+ * Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * The AusStage XML Data Manager Service is distributed in the hope 
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AusStage XML Data Manager Service. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// import java libraries as required
+import java.io.*;
+
+/**
+ * A class used to execute an XPath expression against a given XML file
+ */
+public class XMLDataManager {
+
+	/**
+	 * A method to execute an XPath expression against a supplied XML file
+	 *
+	 * @param xmlFile the XML file to execute the XPath expression against
+	 * @param xpath   the XPath expression to execute
+	 *
+	 * @return the results of the query
+	 */
+	public String executeQuery(File xmlFile, String xpath) {
+
+		// debug code
+		return "";
+
+	} // end executeQuery method
+
+} // end class definition
