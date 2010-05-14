bbadd72069b98370e489da32bf598519a714dee5
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-05-14 02:42:23 +0000

    add XML write capability to the AbsAgeBySex class part of the ABS Data Fix app

--- abs-data-fix/src/AbsAgeBySex.java
+++ abs-data-fix/src/AbsAgeBySex.java
@@ -18,6 +18,12 @@
 
 // import additional packages
 import java.io.*;
+import java.util.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*; 
+import javax.xml.transform.*; 
+import javax.xml.transform.dom.*; 
+import javax.xml.transform.stream.*;
 
 
 /**
@@ -76,15 +82,15 @@ public class AbsAgeBySex {
 		int[]    people = new int[DATA_COLUMNS];
 		String[] ages = {"0-4 years","5-9 years","10-14 years","15-19 years","20-24 years","25-29 years","30-34 years","35-39 years","40-44 years","45-49 years","50-54 years","55-59 years","60-64 years","65-69 years","70-74 years","75-79 years","80-84 years","85-89 years","90-94 years","95-99 years","100 years and over"};
 		
-		// average calculation
-		float avgPartOne = 0;
-		float avgPartTwo = 0;
-		float average    = 0;
-		
 		// results
+		int    average      = 0;
 		String avgMaleAge   = null;
 		String avgFemaleAge = null;
 		String avgTotalAge  = null;
+		
+		// Data Element manage tasks
+		DataElement dataElem = null;
+		HashSet<DataElement> dataElements = new HashSet<DataElement>();
 		
 		// open the input file
 		try {
@@ -134,13 +140,12 @@ public class AbsAgeBySex {
 					district = dataElems[1];
 					district = district.replaceAll("\"", "");
 					
+					// create a new dataElement and add it to the collection
+					dataElem = new DataElement(district);
+					dataElements.add(dataElem);
+					
 					// reset the people array
 					people = new int[DATA_COLUMNS];
-					
-					// reset the average variables
-					avgPartOne = 0;
-					avgPartTwo = 0;
-					average    = 0;
 										
 					// get the male populations
 					for(int i = 0; i < DATA_COLUMNS; i++) {
@@ -152,34 +157,25 @@ public class AbsAgeBySex {
 						}
 					}
 					
-					// get the first part of the average
-					for(int i = 0; i < people.length; i++) {
-						avgPartOne += people[i] * (i + 1);
-					}
-					
-					// get the second part of the average
-					for(int i = 0; i < people.length; i++) {
-						avgPartTwo += people[i];
-					}
-					
-					// get the average
-					average = avgPartOne / avgPartTwo;
+					// calculate the average
+					average = calculateAverage(people);
 					
 					// double check the average
 					if(average > 0) {
 						// get the average male age
-						avgMaleAge = ages[(Math.round(average) - 1)];
+						avgMaleAge = ages[average - 1];
 					} else {
 						avgMaleAge = "N/A";
 					}
 					
+					// save the male average
+					dataElem.setAvgMaleAge(avgMaleAge);
+					
+					// build and save the table
+					dataElem.addHtml(buildTable("Male Age by 5 Year Age Groups (Place of Usual Residence)", people, ages));
+					
 					// reset the people array
 					people = new int[DATA_COLUMNS];
-					
-					// reset the average variables
-					avgPartOne = 0;
-					avgPartTwo = 0;
-					average    = 0;
 					
 					/*
 					 * get the female average age
@@ -194,34 +190,25 @@ public class AbsAgeBySex {
 						}
 					}
 					
-					// get the first part of the average
-					for(int i = 0; i < people.length; i++) {
-						avgPartOne += people[i] * (i + 1);
-					}
-					
-					// get the second part of the average
-					for(int i = 0; i < people.length; i++) {
-						avgPartTwo += people[i];
-					}
-					
-					// get the average
-					average = avgPartOne / avgPartTwo;
+					// calculate the average
+					average = calculateAverage(people);
 					
 					// double check the average
 					if(average > 0) {
 						// get the average male age
-						avgFemaleAge = ages[(Math.round(average) - 1)];
+						avgFemaleAge = ages[average - 1];
 					} else {
 						avgFemaleAge = "N/A";
 					}
 					
+					// save the male average
+					dataElem.setAvgFemaleAge(avgFemaleAge);
+					
+					// build and save the table
+					dataElem.addHtml(buildTable("Female Age by 5 Year Age Groups (Place of Usual Residence)", people, ages));
+					
 					// reset the people array
 					people = new int[DATA_COLUMNS];
-					
-					// reset the average variables
-					avgPartOne = 0;
-					avgPartTwo = 0;
-					average    = 0;
 					
 					/*
 					 * get the total population average age
@@ -236,28 +223,27 @@ public class AbsAgeBySex {
 						}
 					}
 					
-					// get the first part of the average
-					for(int i = 0; i < people.length; i++) {
-						avgPartOne += people[i] * (i + 1);
-					}
-					
-					// get the second part of the average
-					for(int i = 0; i < people.length; i++) {
-						avgPartTwo += people[i];
-					}
-					
-					// get the average
-					average = avgPartOne / avgPartTwo;
+					// calculate the average
+					average = calculateAverage(people);
 					
 					// double check the average
 					if(average > 0) {
 						// get the average male age
-						avgTotalAge = ages[(Math.round(average) - 1)];
+						avgTotalAge = ages[average - 1];
 					} else {
 						avgTotalAge = "N/A";
 					}
 					
-					// keep the user informed
+					// save the male average
+					dataElem.setAvgTotalAge(avgTotalAge);
+					
+					// build and save the table
+					dataElem.addHtml(buildTable("Total Population Age by 5 Year Age Groups (Place of Usual Residence)", people, ages));
+					
+					// reset the people array
+					people = new int[DATA_COLUMNS];
+					
+					// keep the user 
 					System.out.println("INFO: District: " + district + " Avg. Male Age: " + avgMaleAge + " Avg. Female Age: " + avgFemaleAge + " Avg. Population Age: " + avgTotalAge); 			
 					
 					// increment the line counter
@@ -269,10 +255,252 @@ public class AbsAgeBySex {
 			System.err.println("ERROR: Unable to read from the specified file");
 			return false;
 		}
+		
+		// output the XML
+		try {
+			writeXmlDataset(dataElements, output);
+		} catch (Exception ex) {
+			System.err.println("ERROR: Unable to write the XML file");
+			return false;
+		}
 	
 		// debug code
 		return true;
 	
 	} // end doTask method
+	
+	/**
+	 * A method to calculate the average 
+	 *
+	 * @param values an array of values
+	 *
+	 * @return       the calculated average rounded to the nearest int
+	 */
+	private int calculateAverage(int[] values) {
+	
+		// average calculation
+		float avgPartOne = 0;
+		float avgPartTwo = 0;
+		float average    = 0;
+		
+		// get the first part of the average
+		for(int i = 0; i < values.length; i++) {
+			avgPartOne += values[i] * (i + 1);
+		}
+		
+		// get the second part of the average
+		for(int i = 0; i < values.length; i++) {
+			avgPartTwo += values[i];
+		}
+		
+		// get the average
+		average = avgPartOne / avgPartTwo;
+		
+		// double check the average
+		if(average > 0) {
+			// get the average male age
+			return Math.round(average);
+		} else {
+			return 0;
+		}
+	} // end calculateAverage method
+	
+	/**
+	 * A method to build the data table
+	 *
+	 * @param caption the caption of the table
+	 * @param values  the values of the data row of the table
+	 * @param headers the values of the header row
+	 *
+	 * @return the completed table
+	 */
+	private String buildTable(String caption, int[] values, String[] headers) {
+	
+		// declare helper variables
+		StringBuilder table = new StringBuilder();
+		
+		// start the table
+		table.append("<table>\n<caption>" + caption + "</caption>\n");
+		table.append("<thead><tr>");
+		
+		// add the headers
+		for(int i = 0; i < headers.length; i++) {
+			table.append("<th>" + headers[i] + "</th>");
+		}
+		
+		// finalise the header
+		table.append("</tr></thead>\n");
+		
+		// start the body of the table
+		table.append("<tbody><tr>");
+		
+		// add the values
+		for(int i = 0; i < values.length; i++) {
+			table.append("<td>" + Integer.toString(values[i]) + "</td>");
+		}
+		
+		// finalse the table
+		table.append("</tr></tbody>\n</table>\n");
+		
+		// return the generated table
+		return table.toString();
 
+	} // end buildTable method
+	
+	/**
+	 * A method to build and write the XML file
+	 *
+	 * @param dataElements the data elements to use to build the dataset
+	 * @param outputFile   the output file to write the XML to
+	 */
+	private void writeXmlDataset(HashSet<DataElement> dataElements, File outputFile) {
+	
+		// build the marker XML
+		try {
+			// create the xml document object
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder        builder = factory.newDocumentBuilder();
+			Document			   xmlDoc  = builder.newDocument();
+			
+			// add the root element
+			Element rootElement = xmlDoc.createElement("ABSData");
+			xmlDoc.appendChild(rootElement);
+			
+			// get an iterator over the collection
+			Iterator iterator = dataElements.iterator();
+			
+			// iterate over the contributors
+			while(iterator.hasNext()) {
+			
+				// get a data element
+				DataElement dataElem = (DataElement)iterator.next();
+				
+				// build the XML for this collection district
+				Element district = xmlDoc.createElement("district");
+				
+				// add the collection district id
+				district.setAttribute("id", dataElem.getId());
+				
+				// add the averages
+				Element xmlElement = xmlDoc.createElement("AvgMaleAge");
+				xmlElement.setTextContent(dataElem.getAvgMaleAge());
+				district.appendChild(xmlElement);
+				
+				xmlElement = xmlDoc.createElement("AvgFemaleAge");
+				xmlElement.setTextContent(dataElem.getAvgFemaleAge());
+				district.appendChild(xmlElement);
+				
+				xmlElement = xmlDoc.createElement("AvgTotalAge");
+				xmlElement.setTextContent(dataElem.getAvgTotalAge());
+				district.appendChild(xmlElement);
+				
+				// add the description
+				xmlElement = xmlDoc.createElement("table");
+				xmlElement.appendChild(xmlDoc.createCDATASection(dataElem.getHtml()));
+				district.appendChild(xmlElement);
+				
+				// add this element to the tree
+				rootElement.appendChild(district);
+			}
+			
+			// create a transformer 
+			TransformerFactory transFactory = TransformerFactory.newInstance();
+			Transformer        transformer  = transFactory.newTransformer();
+			
+			// set some options on the transformer
+			transformer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+			// get a transformer and supporting classes
+			StringWriter writer = new StringWriter();
+			StreamResult result = new StreamResult(writer);
+			DOMSource    source = new DOMSource(xmlDoc);
+			
+			// transform the xml document into a string
+			transformer.transform(source, result);
+			
+			// open the output file
+			FileWriter output = new FileWriter(outputFile);
+			output.write(writer.toString());
+			output.close();			
+			
+		} catch(javax.xml.parsers.ParserConfigurationException ex) {
+			throw new RuntimeException("ERROR: Unable to build xml", ex);
+		} catch(javax.xml.transform.TransformerException e) {
+			throw new RuntimeException("ERROR: Unable to build xml", e);
+		}catch (java.io.IOException ex) {
+			System.out.println("ERROR: Unable to write xml file\n" + ex);
+		}
+			
+	} // end buildXmlDataset
+
+} // end class definition
+
+/**
+ * A class to manage the age by sex data for a collection district
+ */
+class DataElement {
+
+	// declare private variables
+	private String id;           // collection district id
+	private String avgMaleAge;   // calculated average male age
+	private String avgFemaleAge; // calculated average female age
+	private String avgTotalAge;  // calculated total age
+	private StringBuilder html;  // html version of the data including tables
+
+	/**
+	 * Constructor for this class
+	 *
+	 * @param id the collection district id
+	 */
+	public DataElement (String id) {
+	
+		this.id = id;
+		
+		html = new StringBuilder();
+		
+	} // end constructor
+	
+	/*
+	 * get and set methods
+	 */
+	public String getId() {
+		return id;
+	}
+		 
+	public void setAvgMaleAge(String value) {
+		avgMaleAge = value;
+	}
+	
+	public String getAvgMaleAge() {
+		return avgMaleAge;
+	}
+	
+	public void setAvgFemaleAge(String value) {
+		avgFemaleAge = value;
+	}
+	
+	public String getAvgFemaleAge() {
+		return avgFemaleAge;
+	}
+	
+	public void setAvgTotalAge(String value) {
+		avgTotalAge = value;
+	}
+	
+	public String getAvgTotalAge() {
+		return avgTotalAge;
+	}
+	
+	/*
+	 * html methods
+	 */
+	public void addHtml(String value) {
+		html.append(value);
+	}
+	
+	public String getHtml() {
+		return html.toString();
+	}
 } // end class definition
