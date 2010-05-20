f36da42e338a80844732e6790f13bbe3dc493dea
ren0040@flinders.edu.au <ren0040@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-05-20 00:34:20 +0000

    XMLDataManager class: executeQuery() method will execute an XPath expression against a supplied XML file and return a NodeList result.
    XMLDataServlet : doGet() will get two parameters, one is XML file name and the other is XPath string. Then it will call executeQuery() method in XMLDataManager class and serialize the NodeList result.

--- trunk/xmldata/src/au/edu/ausstage/xmldata/XMLDataManager.java
+++ trunk/xmldata/src/au/edu/ausstage/xmldata/XMLDataManager.java
@@ -16,29 +16,186 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 package au.edu.ausstage.xmldata;
 
-// import java libraries as required
-import java.io.*;
+//import java.io.*;
+//
+//import javax.xml.transform.OutputKeys;
+//import javax.xml.transform.Transformer;
+//import javax.xml.transform.TransformerFactory;
+//import javax.xml.transform.dom.DOMSource;
+//import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+//import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 /**
  * A class used to execute an XPath expression against a given XML file
  */
+
 public class XMLDataManager {
 
 	/**
-	 * A method to execute an XPath expression against a supplied XML file
-	 *
-	 * @param xmlFile the XML file to execute the XPath expression against
-	 * @param xpath   the XPath expression to execute
-	 *
-	 * @return the results of the query
+	 * A method to execute an XPath expression against a supplied XML file and
+	 * return a NodeList result
+	 * 
+	 * @param xmlFile
+	 *            the XML file to execute the XPath expression against
+	 * @param xpathStr
+	 *            the XPath expression to execute
+	 * 
+	 * @return the NodeList result of the query
 	 */
-	public String executeQuery(File xmlFile, String xpath) {
+	public NodeList executeQuery(String xmlFile, String xpathStr)
+			throws Exception {
 
-		// debug code
-		return "";
+		InputSource xml = new InputSource(xmlFile);   
+		
+		// Create a new XPath
+        XPathFactory factory = XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        
+        Object result = null;
+        try {
+          // compile the XPath expression
+          XPathExpression xpathExpr = xpath.compile(xpathStr);
+          
+          // Evaluate the XPath expression against the input document
+          result = xpathExpr.evaluate(xml, XPathConstants.NODESET);
+                    
+          // Print the result to System.out.
+          //printResult(result);
+          
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+        }        
+	    //return result;
+		return (NodeList)result;
 
 	} // end executeQuery method
 
-} // end class definition
+	
+//    /**
+//     * Print the type and value of the evaluation result.
+//     */
+//    static void printResult(Object result)
+//      throws Exception    {
+//        if (result instanceof Double) {
+//            System.out.println("Result type: double");
+//            System.out.println("Value: " + result);
+//        }
+//        else if (result instanceof Boolean) {
+//            System.out.println("Result type: boolean");
+//            System.out.println("Value: " + ((Boolean)result).booleanValue());
+//        }	
+//        else if (result instanceof String) {
+//            System.out.println("Result type: String");
+//             System.out.println("Value: " + result);
+//        }
+//        else if (result instanceof Node) {
+//            Node node = (Node)result;
+//            System.out.println("Result type: Node");
+//            System.out.println("<output>");
+//            printNode(node);
+//            System.out.println("</output>");
+//        }
+//        else if (result instanceof NodeList) {
+//            NodeList nodelist = (NodeList)result;
+//            System.out.println("Result type: NodeList");
+//            System.out.println("<output>");
+//            printNodeList(nodelist);
+//            System.out.println("</output>");
+//        }
+//    }
+//          
+//	/** Decide if the node is text, and so must be handled specially */
+//	static boolean isTextNode(Node n) {
+//		if (n == null)
+//			return false;
+//		short nodeType = n.getNodeType();
+//		return nodeType == Node.CDATA_SECTION_NODE
+//				|| nodeType == Node.TEXT_NODE;
+//	}
+//
+//	static void printNode(Node node) throws Exception {
+//		if (isTextNode(node)) {
+//			System.out.println(node.getNodeValue());
+//		} else {
+//			// Set up an identity transformer to use as serializer.
+//			Transformer serializer = TransformerFactory.newInstance()
+//					.newTransformer();
+//			serializer
+//					.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+//			serializer.transform(new DOMSource(node), new StreamResult(
+//					new OutputStreamWriter(System.out)));
+//		}
+//
+//	}
+//    
+//	static void printNodeList(NodeList nodelist) 
+//    throws Exception
+//    {
+//    	Node n;
+//
+//    	// Set up an identity transformer to use as serializer.
+//    	Transformer serializer = TransformerFactory.newInstance().newTransformer();
+//    	serializer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+//
+//    	for (int i = 0; i < nodelist.getLength(); i++)
+//    	{         
+//    		n = nodelist.item(i);
+//    		if (isTextNode(n)) {
+//    			// DOM may have more than one node corresponding to a 
+//    			// single XPath text node.  Coalesce all contiguous text nodes
+//    			// at this level
+//    			StringBuffer sb = new StringBuffer(n.getNodeValue());
+//    			for (
+//    					Node nn = n.getNextSibling(); 
+//    					isTextNode(nn);
+//    					nn = nn.getNextSibling()
+//    			) {
+//    				sb.append(nn.getNodeValue());
+//    			}
+//    			System.out.print(sb);
+//    		}
+//    		else {
+//    			serializer.transform(new DOMSource(n), new StreamResult(new OutputStreamWriter(System.out)));
+//    		}
+//    		System.out.println();
+//    	}
+//    }
+	
+//	public static void main(String [] args) throws Exception{
+//	
+//		File xmlFile = null;
+//		String xpathStr = null;
+//		
+//		if (args.length != 2) {
+//			System.out.println("Please give the correct number of args");
+//			return;
+//		}
+//		if ((args[0] != null) && (args[0].length() > 0)
+//				            && (args[1] != null) && (args[1].length() > 0)) {
+//						
+//			xmlFile = new File(args[0]);
+//			xpathStr = args[1];
+//		
+////			xmlFile = "foo.xml";
+////			xpathStr ="/doc/name";
+//			
+//			XMLDataManager app = new XMLDataManager();
+//			app.executeComplexQuery(xmlFile, xpathStr);
+//			//app.executeSimpleQuery(xmlFile, xpathStr);
+//			
+//		} else {
+//			System.out.println("args is null or length is 0");
+//		}
+//	}
+}
--- trunk/xmldata/src/au/edu/ausstage/xmldata/XMLDataServlet.java
+++ trunk/xmldata/src/au/edu/ausstage/xmldata/XMLDataServlet.java
@@ -18,20 +18,37 @@
 
 package au.edu.ausstage.xmldata;
 
-// import additional classes
+//import additional classes
 import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * The main driving class for the this service
  */
 public class XMLDataServlet extends HttpServlet {
-
+	
 	// declare private variables
 	private ServletConfig servletConfig;
-
-	/*
+	
+       
+     /*
 	 * initialise this instance
 	 */
 	public void init(ServletConfig conf) throws ServletException {
@@ -49,8 +66,7 @@ public class XMLDataServlet extends HttpServlet {
 	 * @param request a HttpServletRequest object representing the current request
 	 * @param response a HttpServletResponse object representing the current response
 	 */
-	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// get the parameters
 		String xmlFileName = request.getParameter("file");  // parameter to name which XML file to query
 		String xPathString = request.getParameter("xpath"); // xpath parameter to use as the query
@@ -62,61 +78,179 @@ public class XMLDataServlet extends HttpServlet {
 		 * trim the parameters
 		 * check for empty strings
 		 */
+		xmlFileName = xmlFileName.trim();
+		xPathString = xPathString.trim();
+		
+		if ((xmlFileName == null) || (xmlFileName.length() == 0)
+				&& (xPathString == null) || (xPathString.length() == 0)) {
+			throw new IllegalArgumentException(
+            	"null argument or its length is 0 in method call.");
+		}
+		// build file path
 
-		 // build file path
-		 
-		 /*
-		  * need to take into account the path seperator as well before joining the two variables
-		  */
-		  
-		 String rootDirectory = servletConfig.getServletContext().getInitParameter("rootDirectory");
-		 String fullPathToXML = rootDirectory + xmlFileName;
-
-		 // add root directory to file name
-
-		 // check the file
-		 File xmlFile = new File(fullPathToXML);
-
-		 /*
-		 * check that the file exists
-		 * check that the file can be read
+		/*
+		 * need to take into account the path seperator as well before joining
+		 * the two variables
 		 */
 
-		 // get an instance of the XMLDataManager object
-		 XMLDataManager dataManager = new XMLDataManager();
+		String fullPathToXML = null;
+		String rootDirectory = servletConfig.getServletContext()
+				.getInitParameter("rootDirectory");
 
-		 // execute the query against the XML file and get the results
-		 String results = dataManager.executeQuery(xmlFile, xPathString);
+		if (rootDirectory.endsWith(File.separator))
+			// add root directory to file name
+			fullPathToXML = rootDirectory.trim() + xmlFileName;
+		else
+			fullPathToXML = rootDirectory.trim() + File.separator + xmlFileName;
 
-		// return the results to the browser
-		// set the appropriate content type
-		response.setContentType("text/xml; charset=UTF-8");
+		System.out.println("fullPathToXML:============= " + fullPathToXML);
+		System.out.println("xPathString: =============" + xPathString);
+		// check the file
+		//File xmlFile = new File(fullPathToXML);
 
-		// set the appropriate headers to disable caching
-		// particularly for IE
-		response.setHeader("Cache-Control", "max-age=0,no-cache,no-store,post-check=0,pre-check=0");
+		/*
+		 * check that the file exists check that the file can be read
+		 */
 
-		//get the output print writer
-		PrintWriter out = response.getWriter();
+		// get an instance of the XMLDataManager object
+		XMLDataManager dataManager = new XMLDataManager();
 
-		// send some output
-		out.print(results);		 
+		// execute the query against the XML file and get the results
+		String resultStr = null;
+		NodeList resultNodeList = null;
+				
+		try {
+			
+			resultNodeList = dataManager.executeQuery(fullPathToXML,
+					xPathString);
+			if (resultNodeList == null || resultNodeList.getLength() == 0) 
+	             resultStr = "<output>Query result is Null or empty!</output> ";
+			else 
+				 resultStr = serialize(resultNodeList);
+			
+			System.out.println(resultStr);
+			// return the results to the browser
+			// set the appropriate content type
+			response.setContentType("text/xml; charset=UTF-8");
 
+			// set the appropriate headers to disable caching
+			// particularly for IE
+			response.setHeader("Cache-Control",
+					"max-age=0,no-cache,no-store,post-check=0,pre-check=0");
+
+			// get the output print writer
+			PrintWriter out = response.getWriter();
+
+			// send some output
+			out.print(resultStr);
+			out.close();
+	        out.flush();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	} // end doGet method
 
+	/** Decide if the node is text, and so must be handled specially */
+	protected boolean isTextNode(Node n) {
+		if (n == null)
+			return false;
+		short nodeType = n.getNodeType();
+		return nodeType == Node.CDATA_SECTION_NODE
+				|| nodeType == Node.TEXT_NODE;
+	}
+	
+
+	/**
+	 * Serialise the supplied W3C DOM subtree.
+	 * 
+	 * @param nodeList The DOM subtree as a NodeList.
+	 * @throws DOMException Unable to serialise the DOM.
+	 */
+	protected String serialize(NodeList nodeList) {
+
+		if (nodeList == null) {
+			throw new IllegalArgumentException(
+					"null 'subtree' NodeIterator arg in method call.");
+		}
+		
+		StringWriter writer = new StringWriter();
+		writer.write("<output>");
+		try {
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer;
+
+			factory.setAttribute("indent-number", new Integer(4));
+
+			transformer = factory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION,
+					"yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+			int listLength = nodeList.getLength();
+
+			// Iterate through the Node List.
+			for (int i = 0; i < listLength; i++) {
+				Node node = nodeList.item(i);
+
+				if (isTextNode(node)) {
+					writer.write(node.getNodeValue());
+					writer.write(" ");
+				} else if (node.getNodeType() == Node.ATTRIBUTE_NODE) {
+					writer.write(((Attr) node).getValue());
+					writer.write(" ");
+				} else if (node.getNodeType() == Node.ELEMENT_NODE) {
+					transformer.transform(new DOMSource(node),
+							new StreamResult(writer));
+				}
+			}
+		} catch (TransformerConfigurationException tce) {
+			
+			System.out.println("* Transformer Factory error:");
+			System.out.println("  " + tce.getMessage());
+
+			// Use the contained exception, if any
+			Throwable x = tce;
+			if (tce.getException() != null)
+				x = tce.getException();
+			x.printStackTrace();
+		} catch (TransformerException te) {
+			
+			System.out.println("* Transformation error:");
+			System.out.println("  " + te.getMessage());
+
+			// Use the contained exception, if any
+			Throwable x = te;
+			if (te.getException() != null)
+				x = te.getException();
+			x.printStackTrace();
+
+		} catch (Exception e) {
+			
+			DOMException domExcep = new DOMException(
+					DOMException.INVALID_ACCESS_ERR,
+					"Unable to serailise DOM subtree.");
+			domExcep.initCause(e);
+			throw domExcep;
+		}
+        writer.write("</output>");
+        return writer.toString();
+    }// end serialize method
+	
 	/**
 	 * Method to respond to a post request
 	 *
 	 * @param request a HttpServletRequest object representing the current request
 	 * @param response a HttpServletResponse object representing the current response
 	 */
-	public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// shouldn't use post requests with this service
 		throw new ServletException("Incorrect request type detected");
 
-	} // end doPost method
-
+	}// end doPost method
 
 
 } // end class definition
+
