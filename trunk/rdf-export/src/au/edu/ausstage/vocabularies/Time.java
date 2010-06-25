a5ea5e79e23b766802b8cbb7e7041981ddce4230
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-06-25 05:53:59 +0000

    Added the TimeLine ontology to the AusStage Vocabularies package

--- trunk/rdf-export/src/au/edu/ausstage/vocabularies/Time.java
+++ trunk/rdf-export/src/au/edu/ausstage/vocabularies/Time.java
@@ -1,4 +1,20 @@
-/* CVS $Id: $ */
+/*
+ * This file is part of the AusStage RDF Vocabularies Package
+ *
+ * The AusStage RDF Vocabularies Package is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU General 
+ * Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * The AusStage RDF Vocabularies Package is distributed in the hope 
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AusStage RDF Vocabularies Package.  
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
  
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.ontology.*;
