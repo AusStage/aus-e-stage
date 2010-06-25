c47fcf3812a42e112ca8d33e6571b6ed4aed6df2
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-06-25 01:57:42 +0000

    added start of event export code to RdfExport application

--- trunk/rdf-export/src/au/edu/ausstage/vocabularies/FOAF.java
+++ trunk/rdf-export/src/au/edu/ausstage/vocabularies/FOAF.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of the AusStage RDF Export App
+ * This file is part of the AusStage RDF Vocabularies Package
  *
- * The AusStage RDF Export App is free software: you can 
+ * The AusStage RDF Vocabularies Package is free software: you can 
  * redistribute it and/or modify it under the terms of the GNU General 
  * Public License as published by the Free Software Foundation, either 
  * version 3 of the License, or (at your option) any later version.
  *
- * The AusStage RDF Export App is distributed in the hope 
+ * The AusStage RDF Vocabularies Package is distributed in the hope 
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with the AusStage RDF Export App.  
+ * along with the AusStage RDF Vocabularies Package.  
  * If not, see <http://www.gnu.org/licenses/>.
 */
 
