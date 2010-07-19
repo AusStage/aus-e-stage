04bf59c23d67c97fba7fcf39a46bc39fad7b2ee0
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-07-19 02:43:21 +0000

    start adding TDB access to the Networks Service

--- networks/src/au/edu/ausstage/networks/DataManager.java
+++ networks/src/au/edu/ausstage/networks/DataManager.java
@@ -18,11 +18,71 @@
 
 package au.edu.ausstage.networks;
 
+/*
+ * import additional libraries
+ */
+
+// servlets
+import javax.servlet.ServletConfig;
+
+// Jena & TDB related packages
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.tdb.*;
+
+// import the AusStage vocabularies package
+import au.edu.ausstage.vocabularies.*;
+
 /**
- * A class to manage access to data
+ * A class to manage access to data stored in the RDF model
  */
 public class DataManager {
 
+	// declare private class variables
+	ServletConfig servletConfig = null; // store a reference to the servlet config
+	
+	// Jena and TDB related variables
+	String datastorePath = null;
+	Model model = null;
+
+	/** 
+	 * Constructor for this class
+	 */
+	public DataManager (ServletConfig config) {
+	
+		// store a reference to this ServletConfig for later
+		servletConfig = config;
+		
+	} // end constructor
+	
+	/**
+	 * Connect to the RDF datastore
+	 *
+	 * @param  return true if, and only if, the connection was successful
+	 */
+	public boolean connect() {
+	
+		// get the config parameter
+		datastorePath = servletConfig.getServletContext().getInitParameter("pathToTDB");
+		
+		if(datastorePath == null) {
+			return false;
+		}
+		
+		// connect to the model		
+		model = TDBFactory.createModel(datastorePath);
+		
+		// check the model
+		if(model.isEmpty()) {
+			// the model should not be empty
+			return false;
+		}
+		
+		// if we get this far assume everything is OK
+		return true;
+			
+	} // end connect method
 
 
 } // end class definition
