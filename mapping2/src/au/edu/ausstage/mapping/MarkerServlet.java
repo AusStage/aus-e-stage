a67ba129a463d63115cffc334077a7d3829fcd3e
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-08-06 02:01:27 +0000

    change the MappingServlet into a MarkerServlet in the revised Mapping service

---
+++ mapping2/src/au/edu/ausstage/mapping/MarkerServlet.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of the AusStage Mapping Service
+ *
+ * The AusStage Mapping Service is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The AusStage Mapping Service is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AusStage Mapping Service.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+package au.edu.ausstage.mapping;
+
+// import additional AusStage libraries
+import au.edu.ausstage.utils.*;
+
+// import other packages
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+ 
+public class MappingServlet extends HttpServlet {
+
+	// declare private class variables
+	private ServletConfig servletConfig;
+	
+	// declare private class constants 
+
+	/*
+	 * initialise this instance
+	 */
+	public void init(ServletConfig conf) throws ServletException {
+		// execute the parent objects init method
+		super.init(conf);
+		
+		// store configuration for later
+		servletConfig = conf;
+		
+	} // end init method
+	
+	/**
+	 * Method to respond to a get request
+	 *
+	 * @param request a HttpServletRequest object representing the current request
+	 * @param response a HttpServletResponse object representing the current response
+	 */
+	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	} // end doGet method
+	
+	/**
+	 * Method to respond to a post request
+	 *
+	 * @param request a HttpServletRequest object representing the current request
+	 * @param response a HttpServletResponse object representing the current response
+	 */
+	public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		// error for the time being as this isn't a valid request method at present
+		throw new ServletException("Post requests are not supported at this time");
+	
+	} // end doPost method
+
+
+} // end class definition
