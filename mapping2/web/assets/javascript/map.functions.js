4820dd2bc08ee92a15044c4df06bed77b80523a8
ren0040@flinders.edu.au <ren0040@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-08-16 06:02:22 +0000

    Migrate to Google map API V3 progress

--- mapping2/web/assets/javascript/map.functions.js
+++ mapping2/web/assets/javascript/map.functions.js
@@ -20,7 +20,7 @@
  * JavaScript doesn't provide true constants so any variabls in
  * all caps should be considered constants
  */
-var MARKER_BASE_URL = "http://localhost:8181/mapping2/markers?";
+var MARKER_BASE_URL = "http://localhost:8080/mapping2/markers?";
 
 /*
  * A function to retrieve XML using AJAX and prepare the data
@@ -36,7 +36,7 @@ function getMapData(type, id, updateHeader) {
 
 	// declare helper variables
 	var url;
-
+	
 	// determine what type of API call to make
 	if(type == "organisation") {
 		// this is a request for organisation data
@@ -58,6 +58,8 @@ function getMapData(type, id, updateHeader) {
 	
 		// determine if we should update the header
 		if(updateHeader == true) {
+			console.log("Updating the header");
+
 			// extract the entity elements from the XML
 			var entities = data.documentElement.getElementsByTagName("entity");
 	
@@ -77,8 +79,189 @@ function getMapData(type, id, updateHeader) {
 			// the $("#map_name") syntax uses jQuery to locate the element within the page with the id attribute "map_name"
 			$("#map_name").empty(); // empty the contents of this tag
 			$("#map_name").append(header); // append the contents header variable into the tag
-		}
-	});	
+			
+			// create a new map and centre it on australia
+			var latlng = new google.maps.LatLng(-25.947028, 133.209639);
+		    var myOptions = {
+		      zoom: 4,
+		      center: latlng,
+		      mapTypeId: google.maps.MapTypeId.ROADMAP
+		    };
+
+			var map = new google.maps.Map(document.getElementById("map"), myOptions);
+			$("#map").empty();
+			$("#map").append(map);
+			
+			// extract the markers from the xml
+			var markers = data.documentElement.getElementsByTagName("marker");
+			
+			// object to hold marker locations
+			var locations = {};
+			
+			// build a group of markers
+			for (var i = 0; i < markers.length; i++) {
+			
+				// build a hash of this location
+				var lat = parseFloat(markers[i].getAttribute("lat"));
+				var lng = parseFloat(markers[i].getAttribute("lng"));
+				var latlngHash = (lat.toFixed(6) + "" + lng.toFixed(6));
+				latlngHash     = latlngHash.replace(".","").replace(",", "").replace("-","");
+				
+				if(locations[latlngHash] == null) {
+					// not seen this location before
+					// add to hash
+					locations[latlngHash] = true;
+				} else {
+					// have seen this location before
+					// adjust the lat and lng
+					var randomNumber = Math.floor(Math.random()*3) + 1;
+					randomNumber = "0.000" + randomNumber;
+					randomNumber = parseFloat(randomNumber);
+					
+					lat = lat + randomNumber;
+					lng = lng + randomNumber;
+				}
+				
+				// build a latlng object for this marker
+				var latlng = new google.maps.LatLng(lat, lng);
+				
+				// get the event info for this marker
+				var info   = markers[i].textContent;
+				
+				if(typeof(info) == "undefined") {
+					info = markers[i].text;
+				}
+				
+				// get the name, suburb and postcode
+				var venueName   = markers[i].getAttribute("name");
+				var venueSuburb = markers[i].getAttribute("suburb");
+				
+				venueName = venueName + ", " + venueSuburb
+				
+				// get the colour of the icon
+				var eventCount = parseInt(markers[i].getAttribute("events"));
+				var colour;
+			
+				if(eventCount == 1) {
+					colour = "#CCBAD7";
+				}else if(eventCount < 6) {
+					colour = "#9A7BAB";
+				}else if(eventCount > 5 && eventCount < 16) {
+					colour = "#7F649B";
+				}else if(eventCount > 15 && eventCount < 31) {
+					colour = "#69528E";
+				} else {
+					colour = "#4D3779";
+				}
+				
+				var okToAdd = true;
+				
+/*				// filter markers if required
+			
+					// filter markers
+					var okToAdd = false;
+				
+					// filter markers by date
+					if(start != null) {
+						// filter using date
+						var fDate = parseInt(markers[i].getAttribute("fdate"));
+						var lDate = parseInt(markers[i].getAttribute("ldate"));
+					
+						// check on the fdate
+						if((fDate >= start && fDate <= finish) || (lDate >= start && lDate <= finish)) {
+							// filter markers by state
+							if(focus != null && focus != "nolimit") {
+								// use the state to filter
+								var state = markers[i].getAttribute("state")
+					
+								if(focus == "a" && state != "9") {
+									okToAdd = true;
+								}else if(focus == "1" && state == "1") {
+									okToAdd = true;
+								}else if(focus == "2" && state == "2") {
+									okToAdd = true;
+								}else if(focus == "3" && state == "3") {
+									okToAdd = true;
+								}else if(focus == "4" && state == "4") {
+									okToAdd = true;
+								}else if(focus == "5" && state == "5") {
+									okToAdd = true;
+								}else if(focus == "6" && state == "6") {
+									okToAdd = true;
+								}else if(focus == "7" && state == "7") {
+									okToAdd = true;
+								}else if(focus == "8" && state == "8") {
+									okToAdd = true;
+								}else if(focus == "9" && state == "9") {
+									okToAdd = true;
+								}			
+							} else {
+								okToAdd = true;
+							}
+						}
+					
+					} else {
+						// filter markers by state
+						if(focus != null && focus != "nolimit") {
+							// use the state to filter
+							var state = markers[i].getAttribute("state")
+					
+							if(focus == "a" && state != "9") {
+								okToAdd = true;
+							}else if(focus == "1" && state == "1") {
+								okToAdd = true;
+							}else if(focus == "2" && state == "2") {
+								okToAdd = true;
+							}else if(focus == "3" && state == "3") {
+								okToAdd = true;
+							}else if(focus == "4" && state == "4") {
+								okToAdd = true;
+							}else if(focus == "5" && state == "5") {
+								okToAdd = true;
+							}else if(focus == "6" && state == "6") {
+								okToAdd = true;
+							}else if(focus == "7" && state == "7") {
+								okToAdd = true;
+							}else if(focus == "8" && state == "8") {
+								okToAdd = true;
+							}else if(focus == "9" && state == "9") {
+								okToAdd = true;
+							}			
+						} else {
+							okToAdd = true;
+						}
+					}
+*/				
+					// add marker
+					if(okToAdd == true) {
+						var marker = new google.maps.Marker({  
+							   position: latlng,  
+							   map: map,
+							   title: venueName
+							 });
+					}
+
+				}//end of for (group of markers)
+			
+		}// end of if(updateHeader == true)
+	});	//end of $.get()
+}
+
+//build a single marker
+function createMarker(latlng, info, colour, venueName, map) {
+	
+	// make a new icon
+	var newIcon = MapIconMaker.createMarkerIcon({width: 32, height: 32, primaryColor: colour});
+	
+	// make a new marker
+	var marker = new GMarker(latlng, {icon: newIcon, title: venueName});
+	
+	// add an event listener to listen for a click and show the InfoWindow
+	GEvent.addListener(marker, 'click', function() {
+		marker.openInfoWindowHtml(info, {maxWidth:520, maxHeight:400, autoScroll:true});
+	});
+	
+	return marker;
 }
 
 /* 
