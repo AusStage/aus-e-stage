bcd2bdd9d00833a9ef1d89ad1912b021df57a637
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-08-20 05:42:03 +0000

    start work on the live feedback page and supporting servlets for the mobile service'

---
+++ trunk/mobile/src/au/edu/ausstage/mobile/FeedbackServlet.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of the AusStage Mobile Service
+ *
+ * The AusStage Mobile Service is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The AusStage Mobile Service is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AusStage Mobile Service.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+package au.edu.ausstage.mobile;
+
+// import additional AusStage libraries
+import au.edu.ausstage.utils.*;
+
+// import other packages
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+ 
+public class FeedbackServlet extends HttpServlet {
+
+	// declare private class variables
+	private ServletConfig servletConfig;
+
+	/*
+	 * initialise this instance
+	 */
+	public void init(ServletConfig conf) throws ServletException {
+		// execute the parent objects init method
+		super.init(conf);
+		
+		// store configuration for later
+		servletConfig = conf;
+		
+	} // end init method
+	
+	/**
+	 * Method to respond to a get request
+	 *
+	 * @param request a HttpServletRequest object representing the current request
+	 * @param response a HttpServletResponse object representing the current response
+	 */
+	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+	} // end doGet method
+	
+	/**
+	 * Method to respond to a post request
+	 *
+	 * @param request a HttpServletRequest object representing the current request
+	 * @param response a HttpServletResponse object representing the current response
+	 */
+	public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		// don't respond to post requests
+		throw new ServletException("Invalid Request Type");
+	
+	} // end doPost method
+
+} // end class definition
--- trunk/mobile/src/au/edu/ausstage/mobile/GathererServlet.java
+++ trunk/mobile/src/au/edu/ausstage/mobile/GathererServlet.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of the AusStage Mapping Service
+ * This file is part of the AusStage Mobile Service
  *
- * The AusStage Mapping Service is free software: you can redistribute
+ * The AusStage Mobile Service is free software: you can redistribute
  * it and/or modify it under the terms of the GNU General Public License 
  * as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * The AusStage Mapping Service is distributed in the hope that it will 
+ * The AusStage Mobile Service is distributed in the hope that it will 
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with the AusStage Mapping Service.  
+ * along with the AusStage Mobile Service.  
  * If not, see <http://www.gnu.org/licenses/>.
  */
  
