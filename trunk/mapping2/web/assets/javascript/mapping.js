cdacb10ce74bade253d63ce1686827236acad0ae
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-10-26 01:56:49 +0000

    progress commit of the updated ui for the revised mapping service

---
+++ trunk/mapping2/web/assets/javascript/mapping.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the AusStage Mapping Service
+ *
+ * The AusStage Mapping Service is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The AusStage Mapping Service is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AusStage Mapping Service.  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+// show / hide the menu
+$(document).ready(function(){
+	// add a click handler to the hide menu link
+	$("#hidePanel").click(function(){
+		$("#panel").animate({marginLeft:"-175px"}, 500 );
+		$("#sidebar").animate({width:"0px", opacity:0}, 400 );
+		$("#showPanel").show("normal").animate({width:"18px", opacity:1}, 200);
+		$("#main").animate({marginLeft:"30px"}, 500);
+		$("#tabs").animate({width:"100%"});
+	});
+	// add a click handler to the show menu link
+	$("#showPanel").click(function(){
+		$("#main").animate({marginLeft:"200px"}, 200);
+		$("#tabs").animate({width:"100%"});
+		$("#panel").animate({marginLeft:"0px"}, 400 );
+		$("#sidebar").animate({width:"175px", opacity:1}, 400 );
+		$("#showPanel").animate({width:"0px", opacity:0}, 600).hide("slow");
+	});
+});
+ 
+// setup the analytics tabs
+$(document).ready(function() {
+	$("#tabs").tabs();
+});
