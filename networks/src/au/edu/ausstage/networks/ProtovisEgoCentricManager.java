7a913cebd3459795de6f059aa04b0e6b52a1024e
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-10-29 05:43:14 +0000

    update the networks service with actor edge list export

--- networks/src/au/edu/ausstage/networks/ProtovisEgoCentricManager.java
+++ networks/src/au/edu/ausstage/networks/ProtovisEgoCentricManager.java
@@ -45,7 +45,7 @@ public class ProtovisEgoCentricManager {
 	/**
 	 * Constructor for this class
 	 *
-	 * @param RdfDatabase an already instantiated instance of the DataManager class
+	 * @param database an already instantiated instance of the DataManager class
 	 */
 	public ProtovisEgoCentricManager(DataManager database) {	
 		// store a reference to this DataManager for later
