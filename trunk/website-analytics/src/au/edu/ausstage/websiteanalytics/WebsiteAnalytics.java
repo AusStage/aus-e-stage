567a796f8e4afb548a9f9cd14bb222bbaf235f0f
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-11-15 01:22:33 +0000

    progress commit of the WebsiteAnalytics app including user authentication and profile info

---
+++ trunk/website-analytics/src/au/edu/ausstage/websiteanalytics/WebsiteAnalytics.java
@@ -0,0 +1,139 @@
+/*
+ * This file is part of the AusStage Website Analytics app
+ *
+ * The AusStage Website Analytics app is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The AusStage Website Analytics app is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AAusStage Website Analytics app.  
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package au.edu.ausstage.websiteanalytics;
+
+// import additional ausstage packages
+import au.edu.ausstage.utils.*;
+
+/**
+ * Main driving class for the WebsiteAnalytics app
+ */
+public class WebsiteAnalytics {
+
+	// Version information 
+	public  static final String VERSION    = "1.0.0";
+	private static final String BUILD_DATE = "2010-11-11";
+	private static final String INFO_URL   = "http://code.google.com/p/aus-e-stage/wiki/WebsiteAnalytics";
+	public  static final String APP_NAME   = "AusStage Website Analytics";
+	
+	// private helper constants
+	private static final String[] REQD_PROPERTIES = {"config-dir", "username", "password"};
+	private static final String[] TASK_TYPES      = {"account-data", "build-reports"};
+	
+	/**
+	 * Main driving method for the  WebsiteAnalytics app
+	 */
+	public static void main(String args[]) {
+	
+		// output some basic information
+		System.out.println(APP_NAME + " - Gather info from Google Analytics");
+		System.out.println("Version: " + VERSION + " Build Date: " + BUILD_DATE);
+		System.out.println("More Info: " + INFO_URL + "\n");
+		
+		// output the date 	
+	 	System.out.println("INFO: Application Started: " + DateUtils.getCurrentDateAndTime());
+	 	
+	 	// get the command line arguments
+	 	CommandLineParser cli = new CommandLineParser(args);
+	 	
+	 	// process the list of arguments
+	 	String propsPath = cli.getValue("properties");
+	 	String taskType  = cli.getValue("task");
+	 	
+	 	// double check the parameter
+	 	if(InputUtils.isValid(propsPath) == false || InputUtils.isValid(taskType) == false) {
+	 		System.err.println("ERROR: the following parameters are expected:");
+	 		System.err.println("       -task       the task to perform");
+	 		System.err.println("       -properties the location of the properties file");
+	 		System.err.println("Valid task types are:");
+	 		System.err.println(InputUtils.arrayToString(TASK_TYPES));
+	 		System.exit(-1); 		
+	 	}
+	 	
+	 	// double check the task parameter
+	 	if(InputUtils.isValid(taskType, TASK_TYPES) == false) {
+	 		System.err.println("ERROR: invalid task type specified, expected on of:");
+	 		System.err.println(InputUtils.arrayToString(TASK_TYPES));
+	 		System.exit(-1); 		
+	 	}
+	 	
+	 	// load the properties
+	 	PropertiesManager properties = new PropertiesManager();
+	 	
+	 	if(properties.loadFile(propsPath) == false) {
+	 		System.err.println("ERROR: unable to open the specified properties file");
+	 		System.exit(-1);
+	 	}
+	 	
+	 	// check on the properties
+	 	for(int i = 0; i < REQD_PROPERTIES.length; i++) {
+	 		if(InputUtils.isValid(properties.getValue(REQD_PROPERTIES[i])) == false) {
+	 			System.err.println("ERROR: unable to read the '" + REQD_PROPERTIES[i] + "' property");
+	 			System.exit(-1);
+	 		}
+	 	}
+	 	
+	 	// check the config directory if required
+	 	String configPath = null;
+	 	
+	 	if(taskType.equals("build-reports") == true) {
+	 		// get the path to the config files
+		 	configPath = properties.getValue("config-dir");
+		 	
+		 	if(FileUtils.doesDirExist(configPath) == false) {
+		 		System.err.println("ERROR: unable to find the config directory");
+		 		System.exit(-1);
+		 	}
+		 	
+		 	// get a list of config files
+		 	String[] configFiles = FileUtils.getFileNameListByExtension(configPath, ".xml");
+		 	
+		 	if(configFiles.length == 0) {
+		 		System.err.println("ERROR: unable to find any '.xml' files in the config directory");
+		 		System.exit(-1);
+		 	}
+		} else if(taskType.equals("account-data") == true) {
+			// get the account data for this user
+			AccountData data = new AccountData();
+			
+			// Keep the user informed
+			System.out.println("INFO: Attempting Authentication to Google Analytics");
+			
+			if(data.authenticateUser(properties.getValue("username"), properties.getValue("password")) == false) {
+				System.err.println("ERROR: An unexpected error has occured");
+				System.err.println(data.getLastError());
+				System.exit(-1);
+			} else {
+				System.out.println("INFO: Authentication Successful");
+			}
+			
+			String userDetails = data.getUserDetails();
+			System.out.println("INFO: Account Data");
+			
+			if(userDetails == null) {
+				System.err.println("ERROR: An unexpected error has occured");
+				System.err.println(data.getLastError());
+				System.exit(-1);
+			} else {
+				System.out.println(userDetails);
+			}
+		}
+	
+	}
+}
