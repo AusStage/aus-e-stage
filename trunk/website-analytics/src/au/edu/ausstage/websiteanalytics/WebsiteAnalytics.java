764bbf96241956d1d27ca4260b9c0b0d071916b1
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2010-11-16 06:15:17 +0000

    finalise the website analytics app before upload to the server

--- trunk/website-analytics/src/au/edu/ausstage/websiteanalytics/WebsiteAnalytics.java
+++ trunk/website-analytics/src/au/edu/ausstage/websiteanalytics/WebsiteAnalytics.java
@@ -31,7 +31,7 @@ public class WebsiteAnalytics {
 
 	// Version information 
 	public  static final String VERSION    = "1.0.0";
-	private static final String BUILD_DATE = "2010-11-15";
+	private static final String BUILD_DATE = "2010-11-16";
 	private static final String INFO_URL   = "http://code.google.com/p/aus-e-stage/wiki/WebsiteAnalytics";
 	public  static final String APP_NAME   = "AusStage Website Analytics";
 	
