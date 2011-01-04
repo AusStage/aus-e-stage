8be7189a0253f54e17cc8ec9781e82dc91d8ad75
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2011-01-04 03:53:21 +0000

    minor bugfix to the Exchange Analytics app

--- exchange-analytics/run.sh
+++ exchange-analytics/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to ease compiling by inlcuding class path
 MYPATH=`pwd`
-OUTPUT_PATH=$MYPATH/output/exchange-analytics.xml
+OUTPUT_PATH=/opt/local/persistent_data/root-web/exchange-analytics.xml
 cd $MYPATH/bin
 /bin/rm $OUTPUT_PATH
-/usr/bin/java -cp .:$MYPATH/lib/hsqldb.jar ExchangeAnalytics $MYPATH/log-files $MYPATH/output/database $OUTPUT_PATH
+/usr/bin/java -cp .:$MYPATH/lib/* ExchangeAnalytics $MYPATH/log-files $MYPATH/output/database $OUTPUT_PATH
