1512fe05169a72a6360d325148884751a508eb4e
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2011-06-15 03:56:32 +0000

    archive the old exchange-analytics app

--- exchange-analytics/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# script to ease compiling by inlcuding class path
-MYPATH=`pwd`
-OUTPUT_PATH=/opt/local/persistent_data/root-web/exchange-analytics.xml
-cd $MYPATH/bin
-/bin/rm $OUTPUT_PATH
-/usr/bin/java -cp .:$MYPATH/lib/* ExchangeAnalytics $MYPATH/log-files $MYPATH/output/database $OUTPUT_PATH
