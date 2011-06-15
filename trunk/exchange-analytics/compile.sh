4d7bfda50a7e5a636182150c92341cfb16d6cdee
corey.wallis@flinders.edu.au <corey.wallis@flinders.edu.au@0249e412-20f3-11df-a7a9-25a45d9a8882>
2011-06-15 03:56:32 +0000

    archive the old exchange-analytics app

--- trunk/exchange-analytics/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# script to ease compiling by inlcuding class path
-# get the full path
-MYPATH=`pwd`
-/usr/bin/javac -cp .:$MYPATH/lib/hsqldb.jar -d $MYPATH/bin/ -s $MYPATH/src/ $MYPATH/src/*.java
