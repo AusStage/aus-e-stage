c53129f84ca864c7848c20c37f459118708cfa78
williamsbrad6@gmail.com <williamsbrad6@gmail.com@0249e412-20f3-11df-a7a9-25a45d9a8882>
2011-06-24 04:10:56 +0000

    added bookmark functionality

--- trunk/networks-ui/assets/javascript/networks.js
+++ trunk/networks-ui/assets/javascript/networks.js
@@ -23,32 +23,32 @@ $(document).ready(function(){
 	$("#tabs").tabs();
 	
 	// style the buttons
-	$("button, input:submit").button();
+	styleButtons();
 	
 	// associate tipsy with the span element
     $('.use-tipsy').tipsy({live: true})
 	
 	//style the search results accordions - different to legend accordions- legend accordions have been defined by me to be able 
 	//to use anchor links in the header
-	 $(".accordion").accordion({collapsible:true, active:false, autoHeight: false });
+	$(".accordion").accordion({collapsible:true, active:false, autoHeight: false });
 
 	//initialise the search class.
-	 searchObj = new SearchClass();
-	 searchObj.init();
+	searchObj = new SearchClass();
+	searchObj.init();
 	// check to see if this is a persistent link search request
-     searchObj.doSearchFromLink();
+    searchObj.doSearchFromLink();
 	 
-	 viewer = new ContributorViewerClass(); 
+	viewer = new ContributorViewerClass(); 
 
-	 //initialise th viewer class
-	 viewerControl = new ViewerControlClass();
-	 viewerControl.init();	
+	//initialise th viewer class
+	viewerControl = new ViewerControlClass();
+	viewerControl.init();	
 	  
 	// setup the add search result buttons
-    $('.addSearchResult').live('click', addResultsClick);
+	$('.addSearchResult').live('click', addResultsClick);
 
-	 //bind click event to the search tab to hide legend when clicked.
-	 $('#tabs').bind('tabsselect', function(event, ui) {
+	//bind click event to the search tab to hide legend when clicked.
+	$('#tabs').bind('tabsselect', function(event, ui) {
 		if(ui.index == 0){
 			viewer.hideInteraction();	
 		}
@@ -58,105 +58,36 @@ $(document).ready(function(){
 	
 	 });
 	 
-	 //set up the find centre button
-	 $('#find_centre').live('click', function(){viewer.recentre()})
-	 
-	//set up the search help
-	 $("#help_search_div").dialog({
-     	autoOpen: false,
-        height: 400,
-        width: 450,
-		modal: true,
-		buttons: {
-			Close: function() {
-					$(this).dialog('close');
-				}
-    	    },
-	        open: function() {
-
-			},
-			close: function() {
-
-			}
-        });
-
-        $("#show_search_help").click(function() {
-                $("#help_search_div").dialog('open');
-        });
-
-
-	//set up the view event help
-	 $("#help_view_event_div").dialog({
-     	autoOpen: false,
-        height: 450,
-        width: 600,
-		modal: true,
-		buttons: {
-			Close: function() {
-					$(this).dialog('close');
-				}
-    	    },
-	        open: function() {
-
-			},
-			close: function() {
-
-			}
-        });
-
-        $("#view_event_help").live('click', function() {
-                $("#help_view_event_div").dialog('open');
-        });
-
-		//set up the search help
-	 $("#help_view_contributor_div").dialog({
-     	autoOpen: false,
-        height: 400,
-        width: 450,
-		modal: true,
-		buttons: {
-			Close: function() {
-					$(this).dialog('close');
-				}
-    	    },
-	        open: function() {
-
-			},
-			close: function() {
-
-			}
-        });
-
-        $("#view_contributor_help").live('click', function() {
-                $("#help_view_contributor_div").dialog('open');
-        });
-        
-	//set up the about networks
-	 $("#about_networks_div").dialog({
-     	autoOpen: false,
-        height: 400,
-        width: 450,
-		modal: true,
-		buttons: {
-			Close: function() {
-					$(this).dialog('close');
-				}
-    	    },
-	        open: function() {
-
-			},
-			close: function() {
-
-			}
-        });
-
-        $("#about_networks_help").click(function() {
-                $("#about_networks_div").dialog('open');
-        });
-        
-        
-
+	//set up the find centre button
+	$('#find_centre').live('click', function(){viewer.recentre()})
+	
+	//prepare dialogs and associate them to buttons	
+	prepareDialogs();	 
+	
+	//check the url to see if they are using a bookmark
+ 	checkUrl();       
+ 	
 });
+
+//checks the url for task - ego-centric or event-centric and id
+function checkUrl(){
+	
+	var task = getUrlVar('task');
+	var id = getUrlVar('id');
+	var radius_simplify = getUrlVar('rs');
+	//if the id is a number
+	if (!isNaN(id)){		
+		$('#tabs').tabs('select', 1);
+		switch(task){
+			case 'ego-centric': 	
+				viewerControl.displayNetwork('CONTRIBUTOR', id, 0);
+				break;
+			case 'event-centric':
+				viewerControl.displayNetwork('EVENT', id, 0, radius_simplify) 
+				break;				
+		}
+	}
+}
 
 function addResultsClick(event){
 
@@ -183,7 +114,126 @@ function addResultsClick(event){
 				break;
 			}			
 		  	$('#tabs').tabs('select', 1);	
-			viewerControl.displayNetwork('EVENT', viewerControl.selectedEvent.id[0], 1);
+			viewerControl.displayNetwork('EVENT', viewerControl.selectedEvent.id[0], 1, $('#eventDegree option:selected').val());
 			break;
 	}	
+}
+
+function prepareDialogs(){
+	//set up the search help
+	 $("#help_search_div").dialog({
+     	autoOpen: false,
+        height: 400,
+        width: 450,
+		modal: true,
+		buttons: {
+			Close: function() {
+					$(this).dialog('close');
+				}
+    	    },
+	        open: function() {
+
+			},
+			close: function() {
+
+			}
+        });
+
+        $("#show_search_help").click(function() {
+                $("#help_search_div").dialog('open');
+        });
+
+
+	//set up the view event help
+	$("#help_view_event_div").dialog({
+     	autoOpen: false,
+        height: 450,
+        width: 600,
+		modal: true,
+		buttons: {
+			Close: function() {
+					$(this).dialog('close');
+				}
+    	    },
+	    	open: function() {
+
+			},
+			close: function() {
+		}
+    });
+
+	$("#view_event_help").live('click', function() {
+        $("#help_view_event_div").dialog('open');
+    });
+
+	//set up the search help
+	$("#help_view_contributor_div").dialog({
+     	autoOpen: false,
+        height: 400,
+        width: 450,
+		modal: true,
+		buttons: {
+		Close: function() {
+				$(this).dialog('close');
+			}
+        },
+	        open: function() {
+
+		},
+			close: function() {
+		}
+	});
+
+	$("#view_contributor_help").live('click', function() {
+		$("#help_view_contributor_div").dialog('open');
+    });
+        
+	//set up the about networks
+	$("#about_networks_div").dialog({
+     	autoOpen: false,
+        height: 400,
+        width: 450,
+		modal: true,
+		buttons: {
+			Close: function() {
+					$(this).dialog('close');
+				}
+    	    },
+	        open: function() {
+
+			},
+			close: function() {
+
+			}
+        });
+        
+    $("#about_networks_help").click(function() {
+    	$("#about_networks_div").dialog('open');
+	});
+
+
+	//set up the about networks
+	 $("#network_bookmark_div").dialog({
+     	autoOpen: false,
+        height: 400,
+        width: 450,
+		modal: true,
+		buttons: {
+			Close: function() {
+					$(this).dialog('close');
+				}
+    	    },
+	        open: function() {
+
+			},
+			close: function() {
+
+			}
+        });
+
+    $("#bookmarkLink").click(function() {
+    	$("#network_bookmark_div").dialog('open');
+	});
+
+	
 }
